136a74d474cf749413d2827fa9b6b8ef51705650
Thalyson Wilker <thalison.wilker@gmail.com>
2022-05-05 23:05:01 -0300

    install docker and docker-compose

--- setup.sh
+++ setup.sh
@@ -2,4 +2,19 @@ sudo apt install git -y;
 git config --global user.name "Thalyson Wilker";
 git config --global user.email "thalison.wilker@gmail.com";
 
+apt remove docker docker-engine docker.io containerd runc;
+apt autoremove;
+apt update;
+apt install ca-certificates curl gnupg lsb-release -y;
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg;
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
+
+apt update;
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y;
+docker run hello-world;
+
+apt update;
+apt install docker-compose;
